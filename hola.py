be721e454b50058b212aa66e7e939c925431fe8e
oscar <ofgallo@gmail.com>
2021-08-07 15:50:52 -0500

    commit edit 2 feature1

--- hola.py
+++ hola.py
@@ -3,3 +3,4 @@ import os
 print('Hola soy la version inicial')
 print('Hola soy el edit de hotfix')
 print('Hola soy la primera edicion de feature1')
+print('Hola soy la segunda edicion de feature1')
